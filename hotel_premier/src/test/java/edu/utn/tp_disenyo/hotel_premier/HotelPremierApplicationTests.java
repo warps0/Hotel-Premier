c2296eb140f0a6134982d01534a1e32c6598b930
warps0 <matias.trossero.1@gmail.com>
2025-11-07 18:19:59 -0300

    Project Setup Dependencias y estructura base del proyecto

---
+++ hotel_premier/src/test/java/edu/utn/tp_disenyo/hotel_premier/HotelPremierApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utn.tp_disenyo.hotel_premier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelPremierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
