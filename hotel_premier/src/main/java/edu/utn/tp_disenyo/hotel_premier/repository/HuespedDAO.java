3992378627d7ac7b82287349259e4506ed126c09
pinnguino <s.gallardogaston@gmail.com>
2025-11-07 19:43:23 -0300

    Add DAO interface for Huesped and some custom querys

---
+++ hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/repository/HuespedDAO.java
@@ -0,0 +1,23 @@
+package edu.utn.tp_disenyo.hotel_premier.repository;
+
+import edu.utn.tp_disenyo.hotel_premier.model.Huesped;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface HuespedDAO extends JpaRepository<Huesped, Long> {
+
+    public List<Huesped> findByPosIva(String posIva);
+    public List<Huesped> findByFechaNacimiento(LocalDate fechaNacimiento);
+    public List<Huesped> findByNacionalidad(String nacionalidad);
+    public List<Huesped> findByOcupacion(String ocupacion);
+    public List<Huesped> findByNombre(String nombre);
+    public List<Huesped> findByApellido(String apellido);
+    public List<Huesped> findByDocIdentidad(String docIdentidad);
+
+    // public List<Persona> findByNombre(String nombre); TODO: EnumTipoDoc
+
+}
