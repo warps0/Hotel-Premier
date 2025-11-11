2709341d015d453de169f0ec8a89c2d40debb8e2
pinnguino <s.gallardogaston@gmail.com>
2025-11-11 18:24:18 -0300

    Add exception handler
    Added GlobalExceptionHandler for handling the responses for the HTTP requests.
    Added a response for HuespedNotFoundException.

---
+++ hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package edu.utn.tp_disenyo.hotel_premier.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HuespedNotFoundException.class)
+    public ResponseEntity<String> handleHuespedNotFound(HuespedNotFoundException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+}
