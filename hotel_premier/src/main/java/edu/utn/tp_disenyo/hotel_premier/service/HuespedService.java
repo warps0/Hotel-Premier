8c513d30c471bd9db8bc691f96e5cd8201dc7847
pinnguino <s.gallardogaston@gmail.com>
2025-11-11 18:09:02 -0300

    Add CRUD methods for Huesped
    Added the CRUD methods for Huesped in HuespedService and HuespedServiceImpl.
    Added a custom exception HuespedNotFoundException.

--- hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/service/HuespedService.java
+++ hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/service/HuespedService.java
@@ -1,9 +1,17 @@
 package edu.utn.tp_disenyo.hotel_premier.service;
 
+import edu.utn.tp_disenyo.hotel_premier.exception.HuespedNotFoundException;
 import edu.utn.tp_disenyo.hotel_premier.model.Huesped;
 
-public interface HuespedService {
-    public Huesped createHuesped(Huesped huesped) throws Exception;
+import java.util.List;
 
-    //TODO: Servicio CRUD..., Excepciones
+public interface HuespedService {
+
+    // TODO: Manejar excepciones para el método create
+    public Huesped create(Huesped huesped) throws Exception;
+    public List<Huesped> getAll();
+    public Huesped getById(Long id) throws HuespedNotFoundException;
+    public Huesped update(Long id, Huesped huesped) throws HuespedNotFoundException;
+    public void deleteById(Long id) throws HuespedNotFoundException;
+
 }
--- hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/service/HuespedServiceImpl.java
+++ hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/service/HuespedServiceImpl.java
@@ -1,7 +1,9 @@
 package edu.utn.tp_disenyo.hotel_premier.service;
 
+import java.util.List;
 import java.util.Optional;
 
+import edu.utn.tp_disenyo.hotel_premier.exception.HuespedNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,14 +13,47 @@ import lombok.NonNull;
 
 @Service
 public class HuespedServiceImpl implements HuespedService {
-    @Autowired 
-    private HuespedDAO huespedRepository;
 
+    private final HuespedDAO repository;
+
+    @Autowired
+    public HuespedServiceImpl(HuespedDAO repository) {
+        this.repository = repository;
+    }
+
+    // TODO: Manejar excepciones para el método create
     @Override
-    public Huesped createHuesped(@NonNull Huesped huesped) throws Exception {
-        return Optional.ofNullable(huespedRepository.save(huesped)).orElseThrow(
+    public Huesped create(@NonNull Huesped huesped) throws Exception {
+        return Optional.ofNullable(repository.save(huesped)).orElseThrow(
             () -> new Exception("Error al crear HUESPED")
         );
     }
-    
+
+    @Override
+    public List<Huesped> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Huesped getById(Long id) throws HuespedNotFoundException {
+        return repository.findById(id).orElseThrow(() -> new HuespedNotFoundException());
+    }
+
+    // TODO: update Huesped buscando por nombre/apellido/docIdentidad? - Manejar ID
+    @Override
+    public Huesped update(Long id, @NonNull Huesped huesped) throws HuespedNotFoundException {
+        Huesped actual = repository.findById(id).orElseThrow(() -> new HuespedNotFoundException());
+
+        actual.setApellido(huesped.getApellido());
+        actual.setNombre(huesped.getNombre());
+        actual.setDocIdentidad(huesped.getDocIdentidad());
+        return repository.save(huesped);
+    }
+
+    @Override
+    public void deleteById(Long id) throws HuespedNotFoundException {
+        Huesped huesped = repository.findById(id).orElseThrow(() -> new HuespedNotFoundException());
+        repository.delete(huesped);
+    }
+
 }
