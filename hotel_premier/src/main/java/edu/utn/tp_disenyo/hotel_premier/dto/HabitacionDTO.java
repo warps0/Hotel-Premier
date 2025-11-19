11da86841b67f7a9641d85453aa0a8f7c220157f
Elcolora3x <elcolora2x@gmail.com>
2025-11-19 18:58:44 -0300

    Feat: add DTO class for Habitacion model

---
+++ hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/dto/HabitacionDTO.java
@@ -0,0 +1,21 @@
+package edu.utn.tp_disenyo.hotel_premier.dto;
+
+import edu.utn.tp_disenyo.hotel_premier.model.EstadoHabitacion;
+import edu.utn.tp_disenyo.hotel_premier.util.TipoHabitacion;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class HabitacionDTO {
+    private Long id;
+    private TipoHabitacion tipoHabitacion;
+    private List<EstadoHabitacion> historialEstado = new ArrayList<>();
+
+
+}
