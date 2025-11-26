930cb8b5b1c7816f9bf169adad3d56601852475e
warps0 <matias.trossero.1@gmail.com>
2025-11-26 16:54:16 -0300

    Ensuring historialEstado always sorted right and some more clean

--- hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/model/Habitacion.java
+++ hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/model/Habitacion.java
@@ -14,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -38,6 +39,7 @@ public class Habitacion {
     private Piso piso;
     //TODO: Ordenado por fechaInicio
     @OneToMany(mappedBy = "habitacion", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderBy("fechaInicio ASC")
     private List<EstadoHabitacion> historialEstado = new ArrayList<>();
 
     @ManyToMany(mappedBy = "habitaciones")
