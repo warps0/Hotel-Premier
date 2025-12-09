8b7d17900de06d71ae33ee08d21022745da3991b
Elcolora3x <elcolora2x@gmail.com>
2025-12-09 16:08:13 -0300

    feat: add new method findByEstado(EstadoReserva)

--- hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/repository/ReservaDAO.java
+++ hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/repository/ReservaDAO.java
@@ -2,6 +2,7 @@ package edu.utn.tp_disenyo.hotel_premier.repository;
 
 import java.util.List;
 
+import edu.utn.tp_disenyo.hotel_premier.util.EstadoReserva;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -10,5 +11,6 @@ import edu.utn.tp_disenyo.hotel_premier.model.Reserva;
 public interface ReservaDAO extends JpaRepository<Reserva, Long>, JpaSpecificationExecutor<Reserva> {
 
     public List<Reserva> findByNombreOrApellidoOrContacto(String nombre, String apellido, String contacto);
+    public List<Reserva> findByEstado(EstadoReserva estado) throws Exception;
     
 }
