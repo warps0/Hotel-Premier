ceca1159ce6b0a2e94961f21ac4e4a7be0a9d8e9
Elcolora3x <elcolora2x@gmail.com>
2025-12-14 02:08:48 -0300

    feat: add signature for methods createFactura, getAll, getById and asignarResponsablePago

--- hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/service/FacturaService.java
+++ hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/service/FacturaService.java
@@ -1,5 +1,15 @@
 package edu.utn.tp_disenyo.hotel_premier.service;
 
+import edu.utn.tp_disenyo.hotel_premier.dto.FacturaCreateDTO;
+import edu.utn.tp_disenyo.hotel_premier.model.Factura;
+
+import java.util.List;
+
 public interface FacturaService {
-    
+    public Factura createFactura(FacturaCreateDTO factura);
+    //public Factura updateFactura(Factura factura);
+    //public void deleteFactura(Factura factura);
+    public List<Factura> getAll();
+    public Factura getdById(Long id);
+    public Factura asignarResponsablePago(long idResponsable, long idFactura);
 }
