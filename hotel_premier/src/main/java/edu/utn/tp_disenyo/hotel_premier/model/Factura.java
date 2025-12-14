84f46bc1164730791b06f96aaaa93bbc8b132f11
Elcolora3x <elcolora2x@gmail.com>
2025-12-14 02:07:07 -0300

    feat: add new constructor based on class FacturaCreateDTO

--- hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/model/Factura.java
+++ hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/model/Factura.java
@@ -1,12 +1,14 @@
 package edu.utn.tp_disenyo.hotel_premier.model;
 
+import edu.utn.tp_disenyo.hotel_premier.dto.FacturaCreateDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -20,17 +22,18 @@ public class Factura {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private LocalDate fecha_emision;
-    private float costo_habitacion;
-    private float costo_servicios;
-    private char tipo_factura;
+    private LocalDateTime fecha_emision;
+    private char tipo_factura = 'a';
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Persona responsable_pago;
 
     @ManyToOne
     @JoinColumn(name = "reserva")
     private Reserva reserva;
 
     @OneToMany(cascade = CascadeType.ALL)
-    private List<DetalleFactura> detalleFactura;
+    private List<DetalleFactura> detalleFactura = new ArrayList<>();
 
     public DetalleFactura addDetalleFactura(DetalleFactura detalleFactura) {
         this.detalleFactura.add(detalleFactura);
@@ -40,4 +43,15 @@ public class Factura {
     public void deleteDetalleFactura(DetalleFactura detalleFactura) {
         this.detalleFactura.remove(detalleFactura);
     }
+
+    public Factura (FacturaCreateDTO facturaCreateDTO){
+        this.fecha_emision = LocalDateTime.now();
+        if(facturaCreateDTO.getResponsable_pago() instanceof PersonaJuridica){
+            this.tipo_factura = 'b';
+        }
+        this.responsable_pago = null;
+        this.responsable_pago = facturaCreateDTO.getResponsable_pago();
+        this.detalleFactura = facturaCreateDTO.getDetalleFactura();
+    }
 }
+
