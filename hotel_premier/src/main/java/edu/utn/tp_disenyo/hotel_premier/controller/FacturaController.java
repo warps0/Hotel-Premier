63b10284063f86c7be2fe59aba897d808cab75f2
Elcolora3x <elcolora2x@gmail.com>
2025-12-16 04:29:27 -0300

    feat: create controller class for Factura

---
+++ hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/controller/FacturaController.java
@@ -0,0 +1,36 @@
+package edu.utn.tp_disenyo.hotel_premier.controller;
+
+import edu.utn.tp_disenyo.hotel_premier.dto.FacturaCreateDTO;
+import edu.utn.tp_disenyo.hotel_premier.model.Factura;
+import edu.utn.tp_disenyo.hotel_premier.model.PersonaJuridica;
+import edu.utn.tp_disenyo.hotel_premier.service.FacturaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/factura")
+@CrossOrigin(origins = "http://localhost:3000")
+public class FacturaController {
+    private final FacturaService facturaService;
+
+    public FacturaController(FacturaService facturaService) {
+        this.facturaService = facturaService;
+    }
+
+    @GetMapping("/personaJuridica/{razonSocial}")
+    public ResponseEntity<PersonaJuridica>  getPersonaJuridicaByRazonSocial(@PathVariable("razonSocial") String razonSocial){
+        return new ResponseEntity<>(facturaService.getPersonaJuridicaByRazonSocial(razonSocial), HttpStatus.OK);
+    }
+
+    @PostMapping("/personaJuridica")
+    public ResponseEntity<PersonaJuridica> createPersonaJuridica(@RequestBody PersonaJuridica personaJuridica){
+        return new ResponseEntity<>(facturaService.createPersonaJuridica(personaJuridica), HttpStatus.CREATED);
+    }
+
+    @PostMapping
+    public ResponseEntity<Factura> createFactura(@RequestBody FacturaCreateDTO factura){
+        return new ResponseEntity<>(facturaService.createFactura(factura), HttpStatus.CREATED);
+    }
+}
