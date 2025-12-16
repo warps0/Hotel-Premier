652b791f8251ae49e445c7919a2de284188037d6
Elcolora3x <elcolora2x@gmail.com>
2025-12-16 04:27:08 -0300

    feat: add @Repository tag

--- hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/repository/FacturaDAO.java
+++ hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/repository/FacturaDAO.java
@@ -2,6 +2,8 @@ package edu.utn.tp_disenyo.hotel_premier.repository;
 
 import edu.utn.tp_disenyo.hotel_premier.model.Factura;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FacturaDAO extends JpaRepository<Factura, Long> {
 }
