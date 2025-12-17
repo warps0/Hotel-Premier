3239cc6f9c67a97b32b67cb18630a56d3f71e2a2
warps0 <matias.trossero.1@gmail.com>
2025-12-17 17:03:26 -0300

    Minor Fix

--- hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/dto/ReservaDTO.java
+++ hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/dto/ReservaDTO.java
@@ -11,10 +11,12 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 @EqualsAndHashCode
 public class ReservaDTO {
     private Long id;
