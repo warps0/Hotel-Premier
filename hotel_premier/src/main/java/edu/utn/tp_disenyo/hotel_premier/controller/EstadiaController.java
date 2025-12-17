c70272d1920b2ffdb8da2b0cc7b206fa16d595ce
Elcolora3x <elcolora2x@gmail.com>
2025-12-17 19:45:29 -0300

    refactor: add /api to endpoint for more clarity

--- hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/controller/EstadiaController.java
+++ hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/controller/EstadiaController.java
@@ -2,19 +2,15 @@ package edu.utn.tp_disenyo.hotel_premier.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import edu.utn.tp_disenyo.hotel_premier.dto.EstadiaDTO;
 import edu.utn.tp_disenyo.hotel_premier.dto.EstadiaServicioDTO;
 import edu.utn.tp_disenyo.hotel_premier.service.EstadiaService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
-@RequestMapping("/estadia")
+@RequestMapping("/api/estadia")
 @CrossOrigin(origins = "http://localhost:3000")
 public class EstadiaController {
     private final EstadiaService estadiaService;
@@ -33,5 +29,4 @@ public class EstadiaController {
     public List<EstadiaServicioDTO> getServicios(@PathVariable Long idEstadia) {
         return estadiaService.findEstadiaServicioByIdEstadia(idEstadia);
     }
-    
 }
