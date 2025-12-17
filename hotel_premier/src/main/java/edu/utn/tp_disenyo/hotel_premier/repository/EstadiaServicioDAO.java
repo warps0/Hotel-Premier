bfc87dfa500c236a0dd085cd2af2e9cbed4be638
Elcolora3x <elcolora2x@gmail.com>
2025-12-16 22:15:09 -0300

    feat: add repository for EstadiaServicio class

---
+++ hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/repository/EstadiaServicioDAO.java
@@ -0,0 +1,7 @@
+package edu.utn.tp_disenyo.hotel_premier.repository;
+
+import edu.utn.tp_disenyo.hotel_premier.model.EstadiaServicio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadiaServicioDAO extends JpaRepository<EstadiaServicio, Long> {
+}
