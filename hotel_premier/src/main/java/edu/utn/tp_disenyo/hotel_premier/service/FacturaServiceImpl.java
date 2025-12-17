67937c3b24010f688399283fed4c7250d6e83746
Elcolora3x <elcolora2x@gmail.com>
2025-12-16 22:19:19 -0300

    refactor: change createFactura(facturaCreateDTO) implementation based on changes of EstadiaServicio class

--- hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/service/FacturaServiceImpl.java
+++ hotel_premier/src/main/java/edu/utn/tp_disenyo/hotel_premier/service/FacturaServiceImpl.java
@@ -1,16 +1,8 @@
 package edu.utn.tp_disenyo.hotel_premier.service;
 
 import edu.utn.tp_disenyo.hotel_premier.dto.FacturaCreateDTO;
-import edu.utn.tp_disenyo.hotel_premier.model.Estadia;
-import edu.utn.tp_disenyo.hotel_premier.model.Factura;
-import edu.utn.tp_disenyo.hotel_premier.model.Huesped;
-import edu.utn.tp_disenyo.hotel_premier.model.PersonaJuridica;
-import edu.utn.tp_disenyo.hotel_premier.model.Servicio;
-import edu.utn.tp_disenyo.hotel_premier.model.Persona;
-import edu.utn.tp_disenyo.hotel_premier.repository.EstadiaDAO;
-import edu.utn.tp_disenyo.hotel_premier.repository.FacturaDAO;
-import edu.utn.tp_disenyo.hotel_premier.repository.HuespedDAO;
-import edu.utn.tp_disenyo.hotel_premier.repository.PersonaJuridicaDAO;
+import edu.utn.tp_disenyo.hotel_premier.model.*;
+import edu.utn.tp_disenyo.hotel_premier.repository.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,57 +13,75 @@ public class FacturaServiceImpl implements FacturaService {
     private final FacturaDAO repository;
     private final PersonaJuridicaDAO pjRepository;
     private final HuespedDAO hRepository;
-    private final ReservaService reservaService;
     private final EstadiaDAO estadiaRepository;
+    private final EstadiaServicioDAO estadiaServicioRepository;
 
-    public FacturaServiceImpl(FacturaDAO repository, ReservaService reservaService, PersonaJuridicaDAO pjRepository, HuespedDAO hRepository, EstadiaDAO estadiaRepository) {
+    public FacturaServiceImpl(FacturaDAO repository,
+                              PersonaJuridicaDAO pjRepository,
+                              HuespedDAO hRepository,
+                              EstadiaDAO estadiaRepository,
+                              EstadiaServicioDAO estadiaServicioRepository) {
         this.repository = repository;
-        this.reservaService = reservaService;
         this.pjRepository = pjRepository;
         this.hRepository = hRepository;
         this.estadiaRepository = estadiaRepository;
+        this.estadiaServicioRepository = estadiaServicioRepository;
     }
 
-    public Factura createFactura(FacturaCreateDTO factura){
-        //TODO: Ojo sino encuentra PersonaJuridica 
-        Persona responsableDePago = hRepository.findById(factura.getIdResponsableDePago()).orElseThrow();
+    @Override
+    public Factura createFactura(FacturaCreateDTO facturaDTO){
+        //TODO: Ojo sino encuentra PersonaJuridica
+        Persona responsableDePago = hRepository.findById(facturaDTO.getIdResponsableDePago()).orElseThrow();
 
-        Estadia estadia = estadiaRepository.findById(factura.getIdEstadia()).orElseThrow();
+        Estadia estadia = estadiaRepository.findById(facturaDTO.getIdEstadia()).orElseThrow();
 
-        
+        Factura facturaCreada = new Factura(facturaDTO, responsableDePago, estadia);
 
-        Factura facturaCreada = new Factura(factura, responsableDePago);
         estadia.getFacturas().add(facturaCreada);
+
+        if (facturaDTO.getServicios() != null) {
+            for (EstadiaServicio item : facturaDTO.getServicios()) {
+                item.setEstadiaId(facturaDTO.getIdEstadia());
+                estadiaServicioRepository.save(item);
+            }
+        }
+
         estadiaRepository.save(estadia);
 
         return repository.save(facturaCreada);
     }
 
+    @Override
     public List<Factura> getAll(){
         return repository.findAll();
     }
 
+    @Override
     public Factura getdById(Long id){
         return repository.findById(id).get();
     }
 
+    @Override
     public Factura asignarResponsablePago(long idResponsable, long idFactura){
         Factura factura = this.getdById(idFactura);
-        Huesped h = hRepository.getReferenceById(idResponsable);
-        PersonaJuridica pj = pjRepository.getPersonaJuridicaById(idResponsable);
+        Huesped h = hRepository.findById(idResponsable).orElse(null);
+
         if(h == null){
+            PersonaJuridica pj = pjRepository.findById(idResponsable).orElseThrow();
             factura.setResponsableDePago(pj);
         }
         else{
             factura.setResponsableDePago(h);
         }
-        return factura;
+        return repository.save(factura);
     }
 
+    @Override
     public PersonaJuridica getPersonaJuridicaByRazonSocial(String razonSocial){
         return pjRepository.findByRazonSocial(razonSocial);
     }
 
+    @Override
     public PersonaJuridica createPersonaJuridica(PersonaJuridica personaJuridica){
         return pjRepository.save(personaJuridica);
     }
