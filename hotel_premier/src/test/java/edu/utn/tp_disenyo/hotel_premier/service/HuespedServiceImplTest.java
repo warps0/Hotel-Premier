d16b78a923c319cde1afbaa01d4fca340f59a549
warps0 <matias.trossero.1@gmail.com>
2025-12-18 17:48:18 -0300

    feat: Minor fixes Unnecessary Cleanup & fixes at HuespedService when using findByAttributeLike.

--- hotel_premier/src/test/java/edu/utn/tp_disenyo/hotel_premier/service/HuespedServiceImplTest.java
+++ hotel_premier/src/test/java/edu/utn/tp_disenyo/hotel_premier/service/HuespedServiceImplTest.java
@@ -119,8 +119,8 @@ class HuespedServiceImplTest {
         Huesped hPedroPerez = new Huesped(); hPedroPerez.setId(3L); hPedroPerez.setNombre("Pedro"); hPedroPerez.setApellido("Perez");
 
         // El repo devuelve listas mutables (ArrayList) porque el servicio usa retainAll (que modifica la lista in-place)
-        when(repository.findByNombre("Juan")).thenReturn(new ArrayList<>(List.of(hJuanPerez, hJuanLopez)));
-        when(repository.findByApellido("Perez")).thenReturn(new ArrayList<>(List.of(hPedroPerez, hJuanPerez)));
+        when(repository.findByNombreLike("Juan")).thenReturn(new ArrayList<>(List.of(hJuanPerez, hJuanLopez)));
+        when(repository.findByApellidoLike("Perez")).thenReturn(new ArrayList<>(List.of(hPedroPerez, hJuanPerez)));
 
         List<HuespedDTO> result = service.getAll("Juan", "Perez", null, null);
 
@@ -134,7 +134,7 @@ class HuespedServiceImplTest {
     void testGetAllSingleFilter() {
         Huesped h1 = new Huesped(); h1.setId(1L); h1.setNombre("Matias");
 
-        when(repository.findByNombre("Matias")).thenReturn(new ArrayList<>(List.of(h1)));
+        when(repository.findByNombreLike("Matias")).thenReturn(new ArrayList<>(List.of(h1)));
         // No hace falta mockear apellido, doc, etc., si son null en la llamada,
         // pero dentro del if grande, las listas se inicializan vacías.
 
